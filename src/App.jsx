27b7894135d3f48cd6c621de57054bd49f4f1c4b
Aastik <aastikdude7@gmail.com>
2025-02-06 07:31:50 +0530

    feat: Adding length and special characters

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState, useCallback } from "react";
+import "./App.css";
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [characterAllowed, setCharacterAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) str+= "0123456789"
+    if (numberAllowed) str+= "!@#$%^&*()_+"
+
+    for
+    
+  }, [length, numberAllowed, characterAllowed, setPassword]);
+
+  return (
+    <>
+      <h1 className="text-4xl text-center text-white">Password Generator</h1>
+    </>
+  );
+}
+
+export default App;
