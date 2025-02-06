af283eef5468a0addc25811d1259f21de101f33f
Aastik <aastikdude7@gmail.com>
2025-02-06 09:43:07 +0530

    fix: for loop

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     if (characterAllowed) str += "!@#$%^&*()_+";
 
     for (let i = 1; i <= length; i++) {
-      let char = Math.floor(Math.random() * str.length + 1);
+      let char = Math.floor(Math.random() * str.length);
       pass += str.charAt(char);
     }
 
